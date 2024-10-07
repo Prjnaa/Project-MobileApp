01d1ef80fbcea20271834e80f6a2287dc591eedb
itsPrjnaa <88642463+Prjnaa@users.noreply.github.com>
2024-10-07 22:23:29 +0700

    Jetpack Compose Try 1

--- app/src/main/java/com/project/projectmap/MainActivity.kt
+++ app/src/main/java/com/project/projectmap/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.project.projectmap.ui.theme.ProjectmapTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProjectmapTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +40,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainApp(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp, 48.dp)) {
+                OutlinedButton(
+                    onClick = { /*TODO*/ },
+                    border = BorderStroke(1.5.dp, Color.Gray),
+                ) {
+                    Text(text = "Box 1", modifier = Modifier.padding(8.dp, 6.dp))
+                }
+                OutlinedButton(
+                    onClick = { /*TODO*/ },
+                    border = BorderStroke(1.5.dp, Color.Gray),
+                ) {
+                    Text(text = "Box 1", modifier = Modifier.padding(8.dp, 6.dp))
+                }
+            }
+
+        Row(horizontalArrangement = Arrangement.SpaceAround, modifier = Modifier.fillMaxWidth()) {
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                CircularProgressIndicator(
+                    progress = { 0.75f }, strokeWidth = 6.dp, modifier = Modifier.size(48.dp))
+                Text(text = "Carbs")
+            }
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                CircularProgressIndicator(
+                    progress = { 0.75f }, strokeWidth = 6.dp, modifier = Modifier.size(48.dp))
+                Text(text = "Protein")
+            }
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                CircularProgressIndicator(
+                    progress = { 0.75f }, strokeWidth = 6.dp, modifier = Modifier.size(48.dp))
+                Text(text = "Fat")
+            }
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ProjectmapTheme {
-        Greeting("Android")
-    }
+fun MainAppPreview() {
+    ProjectmapTheme { MainApp() }
 }
