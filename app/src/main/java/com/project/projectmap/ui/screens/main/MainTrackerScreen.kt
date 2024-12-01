8cb91ef95fb1981841bf09f5ae386bf9579f990e
itsPrjnaa <88642463+Prjnaa@users.noreply.github.com>
2024-12-01 23:43:02 +0700

    Resolve Commit

--- app/src/main/java/com/project/projectmap/ui/screens/main/MainTrackerScreen.kt
+++ app/src/main/java/com/project/projectmap/ui/screens/main/MainTrackerScreen.kt
@@ -3,11 +3,13 @@ package com.project.projectmap.ui.screens.main
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +17,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -29,6 +33,7 @@ import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -42,9 +47,17 @@ import com.project.projectmap.ui.theme.ProjectmapTheme
 
 @Composable
 @Preview(showBackground = true)
-fun MainTrackerScreenPreviewLight() {
-    ProjectmapTheme(darkTheme = true) {
+fun MainTrackerScreenPreview() {
+    ProjectmapTheme(darkTheme = false) {
         MainTrackerScreen()
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ChallengesPreview() {
+    ProjectmapTheme(darkTheme = false) {
+        ChallengeList()
     }
 }
 
@@ -62,7 +75,7 @@ fun MainTrackerScreen() {
         TopBar()
         CurrentStats()
         Tracker()
-        ChallengeList()
+//        ChallengeList()
     }
 }
 
@@ -82,7 +95,6 @@ fun TopBar(modifier: Modifier = Modifier) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            // User Section
             Row(
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
                 verticalAlignment = Alignment.CenterVertically
@@ -208,9 +220,40 @@ fun Tracker(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ChallengeList(modifier: Modifier = Modifier) {
-    Row() {
+fun ChallengeList() {
+    val items = List(20) { index -> "Item #${index}" } //Sample
 
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 400.dp),
+        color = MaterialTheme.colorScheme.primaryContainer,
+        shape = RoundedCornerShape(16.dp),
+
+        ) {
+        Column(
+            modifier = Modifier
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            Text(
+                text = "Today's Challenges",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            LazyColumn(
+                modifier = Modifier
+                    .padding(vertical = 24.dp)
+                    .border(color = Color.Red, width = 1.dp)
+            ) {
+                itemsIndexed(items) { index, item ->
+                    ChallengeItem(index = index, text = item, isDone = false)
+                }
+            }
+        }
     }
 }
 
@@ -330,5 +373,30 @@ fun MacroItem(title: String, progress: Float) {
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
         )
+    }
+}
+
+//CHALLENGE CARD
+@Composable
+fun ChallengeItem(index: Int, text: String, isDone: Boolean) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(Color.LightGray),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Index $index: ",
+                modifier = Modifier.padding(end = 8.dp)
+            )
+            Text(
+                text = text,
+            )
+        }
     }
 }
